bc20ea051a44a3bae5fca95a17d29301f096e222
Armani Rodriguez <armani@armanis-mbp.mynetworksettings.com>
2023-07-15 15:43:44 -0400

    fixed docker run script

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,9 @@
 
 # Checks if there is a docker container called "myos" and runs
 # docker build if not foundmake
-#if [[ "$(docker images -q myos 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q myos 2> /dev/null)" == "" ]]; then
   docker build . -t myos --platform linux/amd64
-#fi
+fi
 
 # Compile
 docker run --platform linux/amd64 --rm -it -v $(pwd):/root/env myos
